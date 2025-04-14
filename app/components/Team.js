111bf80512aa30986c8dc9cb855ac3adc02fe7e9
Muhamad Usri Yusron <muhamad.usri@mhs.itenas.ac.id>
2025-04-14 20:32:04 +0700

    Implement code changes to enhance functionality and improve performance

--- app/components/Team.js
+++ app/components/Team.js
@@ -9,35 +9,35 @@ export default function Team() {
             "The attention to detail and innovative features have completely transformed our workflow. This is exactly what we've been looking for.",
           name: "Zahra Cahya",
           designation: "Informatics Student at ITENAS",
-          src: "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?q=80&w=3560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+          src: "/zahra.jpeg",
         },
         {
           quote:
             "Implementation was seamless and the results exceeded our expectations. The platform's flexibility is remarkable.",
           name: "Ambar Wulandari",
           designation: "Informatics Student at ITENAS",
-          src: "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+          src: "/ambar.jpeg",
         },
         {
           quote:
             "This solution has significantly improved our team's productivity. The intuitive interface makes complex tasks simple.",
           name: "Maysa Nusi",
           designation: "Informatics Student at ITENAS",
-          src: "https://images.unsplash.com/photo-1623582854588-d60de57fa33f?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+          src: "/agum.jpeg",
         },
         {
           quote:
             "Outstanding support and robust features. It's rare to find a product that delivers on all its promises.",
           name: "Usri Yusron",
           designation: "Informatics Student at ITENAS",
-          src: "https://images.unsplash.com/photo-1636041293178-808a6762ab39?q=80&w=3464&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+          src: "/gw.jpg",
         },
         {
           quote:
             "The scalability and performance have been game-changing for our organization. Highly recommend to any growing business.",
           name: "Risha Marshela",
           designation: "Informatics Student at ITENAS",
-          src: "https://images.unsplash.com/photo-1624561172888-ac93c696e10c?q=80&w=2592&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+          src: "/risha.jepg",
         },
       ];
       return <AnimatedTestimonials testimonials={testimonials} />;
