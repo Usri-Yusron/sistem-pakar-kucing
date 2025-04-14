f80b3af6448b77c1320a8a97d76be4629525204c
Muhamad Usri Yusron <muhamad.usri@mhs.itenas.ac.id>
2025-04-14 20:33:46 +0700

    Fix image paths in Team component for correct profile references

--- app/components/Team.js
+++ app/components/Team.js
@@ -9,35 +9,35 @@ export default function Team() {
             "The attention to detail and innovative features have completely transformed our workflow. This is exactly what we've been looking for.",
           name: "Zahra Cahya",
           designation: "Informatics Student at ITENAS",
-          src: "/zahra.jpeg",
+          src: "/profile/zahra.jpeg",
         },
         {
           quote:
             "Implementation was seamless and the results exceeded our expectations. The platform's flexibility is remarkable.",
           name: "Ambar Wulandari",
           designation: "Informatics Student at ITENAS",
-          src: "/ambar.jpeg",
+          src: "/profile/ambar.jpeg",
         },
         {
           quote:
             "This solution has significantly improved our team's productivity. The intuitive interface makes complex tasks simple.",
           name: "Maysa Nusi",
           designation: "Informatics Student at ITENAS",
-          src: "/agum.jpeg",
+          src: "/profile/agum.jpeg",
         },
         {
           quote:
             "Outstanding support and robust features. It's rare to find a product that delivers on all its promises.",
           name: "Usri Yusron",
           designation: "Informatics Student at ITENAS",
-          src: "/gw.jpg",
+          src: "/profile/gw.jpg",
         },
         {
           quote:
             "The scalability and performance have been game-changing for our organization. Highly recommend to any growing business.",
           name: "Risha Marshela",
           designation: "Informatics Student at ITENAS",
-          src: "/risha.jepg",
+          src: "/profile/risha.jepg",
         },
       ];
       return <AnimatedTestimonials testimonials={testimonials} />;
